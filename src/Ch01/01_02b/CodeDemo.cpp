9a5af13f8cf08f6b9af7e2286e02dfadf6a2ee69
GOGOTHEDODO <163759871+GOGOTHEDODO@users.noreply.github.com>
2024-03-18 18:14:54 +0000

    added commit

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+
+int main (){ // main needs to return an int in c++
+    std :: cout << "Hi there! " << std::endl; 
+
+    std:: cout << "\n"; //is the same as 
+    std:: cout << std::endl; //but endl flushes the input buffer so is better pratice
+    return(0);  
+} 
